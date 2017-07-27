71277b33908ff36062f6766418d91a723c815ef6
MasterUNG <phrombutr@gmail.com>
2017-07-27 16:38:20 +0700

    Create Class OK

---
+++ Funny Number/MyClass.swift
@@ -0,0 +1,16 @@
+//
+//  MyClass.swift
+//  Funny Number
+//
+//  Created by MasterUNG on 7/27/2560 BE.
+//  Copyright © 2560 MasterUNG. All rights reserved.
+//
+
+import Foundation
+
+class MyClass {
+    
+    let strName = "Doramon"
+    
+    
+}   // MyClass
